78fd0c54a86113805e26f033a9fe1415caa421f5
rxjoicn <josh.cheng@kingsway.school.nz>
2025-10-10 10:41:13 +1300

    Add files via upload

---
+++ variables ver 1.1.py
@@ -0,0 +1,23 @@
+# Author: Josh Cheng
+# Date: 2025-10-8
+# Yr 10 DGT
+# Version 1.1
+'''Changelog:
+ - wrote small program that asks whether the user likes coffee or not and gives answers accordingly
+'''
+
+import sys
+
+opinion = input("Do you like coffee? Yes or No")
+if opinion == "Yes":
+    print("Thats great!")
+elif opinion == "No":
+    print("You should really try some coffee")
+else:
+    print("ERROR, Deleting ALL .sys Files On Your Computer")
+    sys.exit()
+ 
+
+
+    
+    
